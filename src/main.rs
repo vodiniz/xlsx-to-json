7e16db0756e6d40fa9610da82fac98b5799c618c
vodiniz <vitor.oliveira.vod@gmail.com>
2022-03-07 22:17:16 -0300

    range working -> correct function

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,4 @@
-use calamine::{open_workbook, Error, Xlsx, Reader, RangeDeserializerBuilder};
-use office::{Excel, Range, DataType};
-
-fn office_test() {
-
-
-
-    // opens a new workbook
-    let mut workbook = Excel::open("test.xlsx").unwrap();
-    
-    // Read whole worksheet data and provide some statistics
-    if let Ok(range) = workbook.worksheet_range("Sheet1") {
-        let total_cells = range.get_size().0 * range.get_size().1;
-        let non_empty_cells: usize = range.rows().map(|r| {
-            r.iter().filter(|cell| cell != &&DataType::Empty).count()
-        }).sum();
-        println!("Found {} cells in 'Sheet1', including {} non empty cells",
-                 total_cells, non_empty_cells);
-    }
-    
-    // Check if the workbook has a vba project
-    if workbook.has_vba() {
-        let mut vba = workbook.vba_project().expect("Cannot find VbaProject");
-        let vba = vba.to_mut();
-        let module1 = vba.get_module("Module 1").unwrap();
-        println!("Module 1 code:");
-        println!("{}", module1);
-        for r in vba.get_references() {
-            if r.is_missing() {
-                println!("Reference {} is broken or not accessible", r.name);
-            }
-        }
-    }
-}
+use calamine::{open_workbook, Xlsx, Reader,Range, DataType};
 
 
 fn calamine() {
@@ -46,23 +13,33 @@ fn calamine() {
     //             println!("row={:?}, row[0]={:?}", row, row[0]);
     //         }
     //     }
-    // }
-    if let Some(Ok(r)) = excel.worksheet_range(&sheets[0]) {
-        for row in r.rows() {
+    // }1
 
-        println!("row={:?}, row[0]={:?}", row, row[0]);
-        println!("\n --------------------------------------")
+    if let Some(Ok(document)) = excel.worksheet_range(&sheets[0]) {
+
+
+        for column in document.range((0,2),(28,2)).used_cells() {
+            println!("Valor: {:?}", column);
+            
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
 
         }
+            // println!("{:?}", cell);
+
     }
+
+            // println!("row={:?}", row[0]);
+            // println!("\n --------------------------------------")
+
 
 
 }
+        
+
 
 fn main() {
 
-
-    //office_test();
     calamine();
 
 }
