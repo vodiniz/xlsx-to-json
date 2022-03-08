842cfff539bbd016c797ab7a292e6d036f605777
vodiniz <vitor.oliveira.vod@gmail.com>
2022-03-08 14:35:27 -0300

    xlsx extractor working -> route list/pokemon list

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,121 @@
-use calamine::{open_workbook, Xlsx, Reader,Range, DataType};
+use calamine::{open_workbook, Xlsx, Reader, Range};
+use serde::{Serialize, Deserialize};
+use serde_derive::{Serialize, Deserialize};
+use std::fs::File;
+// const EXPECTED_JSON:&str = r#"
+
+// {
+//     "pokemonlocations" : [
+//         {
+//             "encountermethod" : "Grass",
+//             "routes" : [
+//                 {
+//                     "route" : "1",
+//                     "encounters" : {
+//                         "always" : [
+//                             {
+//                                 "pokemon" : "Bidoof",
+//                                 "encounterrate" : 0.1
+
+//                             },
+//                             {
+//                                 "pokemon" : "Starly",
+//                                 "encounterrate" : 0.1
+//                             }
+//                         ]
+//                         "day" : [
+//                             {
+//                                 "pokemon" : "Bidoof",
+//                                 "encounterrate" : 0.1
+//                             }
+//                         ]
+//                     }
+//                 },
+//                 {
+//                     "route" : "Viridian City"
+//                 }
+
+//             ]
+//         }
+//     ]
+// }
+
+// "#;
 
 
-fn calamine() {
 
+#[derive(Serialize, Deserialize)]
+struct Pokemon {
+    pokemon:String,
+    encounterrate:f32
+}
+
+#[derive(Serialize, Deserialize)]
+struct EncounterTime {
+    always:Vec<Pokemon>,
+    day:Vec<Pokemon>,
+    night:Vec<Pokemon>
+
+}
+
+#[derive(Serialize, Deserialize)]
+struct PokemonEncounters{
+    route:String,
+    encountertime:EncounterTime
+}
+
+#[derive(Serialize, Deserialize)]
+struct EncounterMethod {
+
+    encountermethod:String,
+    routes:Vec<PokemonEncounters>
+}
+
+#[derive(Serialize, Deserialize)]
+struct PokemonLocations {
+    pokemonlocation:Vec<EncounterMethod>
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+fn calamine_grass_extractor(column_number: u32) {
+
+    
     let mut excel: Xlsx<_> = open_workbook("pokemon_locations.xlsx").unwrap();
     let sheets = excel.sheet_names().to_owned();
     
-    // for s in sheets {
-    //     println! ("Current Sheet : {}", s);
-    //     if let Some(Ok(r)) = excel.worksheet_range(&s) {
-    //         for row in r.rows() {
-    //             println!("row={:?}, row[0]={:?}", row, row[0]);
-    //         }
-    //     }
-    // }1
-
     if let Some(Ok(document)) = excel.worksheet_range(&sheets[0]) {
 
+        for (column,rate) in (document.range((0,column_number),(28,column_number)).cells()).zip(document.range((0,0),(28,0)).used_cells()) {
+            let pokemon_name = column.2;
+            let pokemon_encounterrate = rate.2;
 
-        for column in document.range((0,2),(28,2)).used_cells() {
-            println!("Valor: {:?}", column);
-            
-            let mut input = String::new();
-            std::io::stdin().read_line(&mut input).unwrap();
+            println!("Pokemon name: {}, encounter rate: {}",pokemon_name, pokemon_encounterrate);
+
 
         }
-            // println!("{:?}", cell);
-
+                    
     }
+    
+}   
 
-            // println!("row={:?}", row[0]);
-            // println!("\n --------------------------------------")
 
-
+fn get_input() -> String {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    
+    return input;
 
 }
+
         
 
 
-fn main() {
 
-    calamine();
+fn main() {
+    let x:u32 = 1;
+    calamine_grass_extractor(x);
 
 }
