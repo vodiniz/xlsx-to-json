8b95da9fc28a9f75df5a5e79e44a2f4ad302ecf8
vodiniz <vitor.oliveira.vod@gmail.com>
2022-03-10 17:53:36 -0300

    day night vector done. -> Compare vector and get day night and always.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use serde_derive::{Serialize, Deserialize};
 use serde_json::json;
 use std::fs::File;
 use std::io::Read;
-
-
+use differ::{Differ, Tag};
 
 #[derive(Serialize, Deserialize)]
 struct Pokemonname{
@@ -21,12 +20,10 @@ struct Pokemonlist {
     results:Vec<Pokemonname>
 }
 
-
-
 #[derive(Serialize, Deserialize)]
 struct Pokemon {
     pokemon:String,
-    encounterrate:f32
+    encounterrate:f64
 }
 
 #[derive(Serialize, Deserialize)]
@@ -34,7 +31,6 @@ struct EncounterTime {
     always:Vec<Pokemon>,
     day:Vec<Pokemon>,
     night:Vec<Pokemon>
-
 }
 
 #[derive(Serialize, Deserialize)]
@@ -50,22 +46,22 @@ struct EncounterMethod {
     routes:Vec<PokemonEncounters>
 }
 
-
 fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
 }
 
-
 fn get_input() -> String {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
-    
     return input;
+}
+
+fn get_day_only_pokemons<T>(day_vec: &Vec<T>, night_vec: &Vec<T>) -> Vec<Vec<T>>{
+
 
 }
 
-
-fn print_example_json() {
+fn print_example_json(){
 
 
     let expected_json:&str = r#"
@@ -108,8 +104,7 @@ fn print_example_json() {
 
 }
 
-
-fn get_pokemon_list () {
+fn get_pokemon_list () -> Vec<Pokemonname>{
     
     let mut json_file = std::fs::File::open("all_pokemon.json").unwrap();
     let mut buff = String::new();
@@ -117,36 +112,72 @@ fn get_pokemon_list () {
     let json: Pokemonlist = serde_json::from_str(&buff).unwrap();
     let pokemons = json.results;
 
-    // return pokemons;   -> COrrigir Erro
+    return pokemons;
 }
 
 
-fn calamine_grass_extractor(column_number: u32) {
+fn calamine_grass_extractor() {
 
-    
     let mut excel: Xlsx<_> = open_workbook("pokemon_locations.xlsx").unwrap();
     let sheets = excel.sheet_names().to_owned();
     
     if let Some(Ok(document)) = excel.worksheet_range(&sheets[0]) {
 
-        for (column,rate) in (document.range((0,column_number),(28,column_number)).cells()).zip(document.range((0,0),(28,0)).used_cells()) {
-            let pokemon_name = column.2;
-            let pokemon_encounterrate = rate.2;
+        let mut column_number: u32 = 1;
+        let mut cell_not_empty = true;
 
-            println!("Pokemon name: {}, encounter rate: {}",pokemon_name, pokemon_encounterrate);
+        while  cell_not_empty{
+            //let pokemon_encounters: PokemonEncounters;
+            let mut day_pokemon_vector = vec![];
+            let mut night_pokemon_vector = vec![];
 
+            let route_name = &(document.range((1,column_number),(1,column_number))[0]);
+            let route_name = route_name[0]     .get_string()   .unwrap();
 
+            for (pokemon_column,rate_column) in (document.range((2,column_number),(13,column_number)).cells()).zip(document.range((2,0),(13,0)).used_cells()) {
+
+                let pokemon_name = pokemon_column.2
+                    . get_string()
+                    .expect("Failed to read Pokemon name")  
+                    .to_string();
+
+                let pokemon_encounterrate = rate_column.2
+                    .get_float()
+                    .expect("Failed to read Pokemon encounter rate");
+
+                //println!("Pokemon name: {}, encounter rate:{}",pokemon_name, pokemon_encounterrate);
+                day_pokemon_vector.push(Pokemon{pokemon: pokemon_name, encounterrate:pokemon_encounterrate});
+            }
+
+            for (pokemon_column,rate_column) in (document.range((16,column_number),(27,column_number)).cells()).zip(document.range((2,0),(13,0)).used_cells()) {
+
+                let pokemon_name = pokemon_column.2
+                    . get_string()
+                    .expect("Failed to read Pokemon name")
+                    .to_string();
+
+                let pokemon_encounterrate = rate_column.2
+                    .get_float()
+                    .expect("Failed to read Pokemon encounter rate");
+                //println!("Pokemon name: {}, encounter rate:{}",pokemon_name, pokemon_encounterrate);
+                
+                night_pokemon_vector.push(Pokemon{pokemon: pokemon_name, encounterrate:pokemon_encounterrate});
+            }
+
+            println!("{:?}", day_pokemon_vector[10].pokemon);
+            column_number += 1;
+            cell_not_empty = false;
         }
-                    
     }
+                    
+}
     
-}   
+   
 
 
 
 fn main() {
-    let x:u32 = 1;
-    //calamine_grass_extractor(x);
-    get_pokemon_list();
 
+    let list = get_pokemon_list();
+    calamine_grass_extractor();
 }
