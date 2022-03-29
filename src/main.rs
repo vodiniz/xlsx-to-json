5607793211fdc9ce1c051d2a87abb554eeab9ecd
vodiniz <vitor.oliveira.vod@gmail.com>
2022-03-29 19:43:13 -0300

    added Fossil pokemons -> next: Unobtainable Pokemons

--- src/main.rs
+++ src/main.rs
@@ -65,6 +65,21 @@ impl fmt::Debug for PokemonTrade {
 }
 
 #[derive(Serialize, Deserialize, Clone)]
+struct EggVendor {
+    pokemon: String,
+    price: String
+}
+
+impl fmt::Debug for EggVendor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "\nPokemon: {}", self.pokemon);
+        write!(f, "\n   Price: {}\n",self.price)
+    }
+}
+
+
+
+#[derive(Serialize, Deserialize, Clone)]
 struct PokemonGift {
     pokemon: String,
     gift_requirement: String
@@ -77,6 +92,28 @@ impl fmt::Debug for PokemonGift {
     }
 }
 
+#[derive(Serialize, Deserialize, Clone)]
+struct Fossil {
+    pokemon: String,
+    price: String,
+    obs: String
+}
+
+impl fmt::Debug for Fossil {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Pokemon: {}", self.pokemon);
+        write!(f, "\n   Price: {}\n",self.price);
+        if self.obs.len() > 0 {
+            write!(f, "\n   Observation: {}\n",self.obs);
+        }
+        write!(f, "\n")
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct UnobtainablePokemon {
+    pokemon:String,
+}
 
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -139,6 +176,32 @@ impl fmt::Debug for EncounterTimeWater {
     }
 
 }
+#[derive(Serialize, Deserialize, Clone)]
+struct EggEncounter {
+    mansion:Vec<EggVendor>,
+    gamecorner:Vec<EggVendor>,
+}
+impl fmt::Debug for EggEncounter {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use `self.number` to refer to each positional data point.
+        write!(f, "\nCeladon Mansion: {:?}\n", self.mansion);
+        write!(f, "\nCeladon Game Corner: {:?}\n",self.gamecorner)
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct FossilEncounter {
+    trade:Vec<Fossil>,
+    found:Vec<Fossil>,
+}
+impl fmt::Debug for FossilEncounter {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use `self.number` to refer to each positional data point.
+        write!(f, "\nTraded Fossil: {:?}\n", self.trade);
+        write!(f, "\nFound Fossil: {:?}\n",self.found)
+    }
+}
+
 
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -173,6 +236,22 @@ impl fmt::Debug for RouteInfo<PokemonGift> {
         write!(f, "\nRoute:{} \n\n{:?}", self.route, self.encounter_info)
     }
 }
+
+impl fmt::Debug for RouteInfo<FossilEncounter> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use `self.number` to refer to each positional data point.
+        write!(f, "\nRoute:{} \n\n{:?}", self.route, self.encounter_info)
+    }
+}
+
+impl fmt::Debug for RouteInfo<EggEncounter> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use `self.number` to refer to each positional data point.
+        write!(f, "\nRoute:{} \n{:?}", self.route, self.encounter_info)
+        
+    }
+}
+
 
  
 #[derive(Serialize, Deserialize, Clone)]
@@ -215,6 +294,29 @@ impl fmt::Debug for Encounters<RouteInfo<PokemonTrade>> {
     }
 }
 impl fmt::Debug for Encounters<RouteInfo<PokemonGift>> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use `self.number` to refer to each positional data point.
+        for all_routes in &self.routes {
+            write!(f, "{:?}", all_routes);
+            write!(f,"------------------------------");
+        }
+        write!(f,"\n")
+    }
+}
+
+impl fmt::Debug for Encounters<RouteInfo<FossilEncounter>> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use `self.number` to refer to each positional data point.
+        for all_routes in &self.routes {
+            write!(f, "{:?}", all_routes);
+            write!(f,"------------------------------");
+        }
+        write!(f,"\n")
+    }
+}
+
+
+impl fmt::Debug for Encounters<RouteInfo<EggEncounter>> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         // Use `self.number` to refer to each positional data point.
         for all_routes in &self.routes {
@@ -386,6 +488,16 @@ fn get_route_name(column_number: u32, document: &calamine::Range<calamine::DataT
     }
 }
 
+fn get_cell_text(row_number:u32, column_number: u32, document: &calamine::Range<calamine::DataType>) -> Option<String>{
+
+    let document_range = document.range((row_number, column_number), (row_number, column_number));
+    let route_str = document_range[0][0].get_string();
+    match route_str {
+        Some(&ref route_str) => Some(route_str.to_string()),
+        _ => None,
+    }
+}
+
 
 fn calamine_water_extractor() -> Option<Encounters<RouteInfo<EncounterTimeWater>>>{
     let mut excel: Xlsx<_> = open_workbook("pokemon_locations.xlsx").expect("Couldn't open pokemon xlsx");
@@ -541,7 +653,6 @@ fn calamine_gift_pokemon_extractor() -> Encounters<RouteInfo<PokemonGift>>{
     if let Some(Ok(document)) = excel.worksheet_range(&sheets[3]){
         let mut row_number = 1;
         loop {
-
             match get_trade_pokemon(row_number, &document){
                 Some(tuple) => {
                     let trade_pokemon: PokemonGift = PokemonGift{pokemon: replace_galarian_alola(&tuple.0) , gift_requirement: replace_galarian_alola(&tuple.2)};
@@ -556,9 +667,150 @@ fn calamine_gift_pokemon_extractor() -> Encounters<RouteInfo<PokemonGift>>{
     }
     let encounters:Encounters<RouteInfo<PokemonGift>> = Encounters { encountermethod: "Trade".to_string(), routes: route_vec };
     encounters
+}
+
+fn get_egg_vendor_pokemon( document: &calamine::Range<calamine::DataType>) -> Option<(Vec<EggVendor>, Vec<EggVendor>)>{
+
+    let mut egg_vendor_mansion:Vec<EggVendor> = vec![];
+    let mut egg_vendor_gamecorner:Vec<EggVendor> = vec![];
+    let mut column_number = 0;
+
+    while column_number < 3{
+        let shard_type = get_cell_text(2, column_number, document);
+        match shard_type{
+            Some(shard_str) => {
+                for pokemon_cell in document.range((3,column_number),(8,column_number)).used_cells(){
+                    if pokemon_cell.0 > 3 {
+                        break;
+                    }
+                    match pokemon_cell.2.get_string(){
+                        Some(pokemon) => {
+                            let pokemon_egg: EggVendor = EggVendor { pokemon: replace_galarian_alola(pokemon), price: format!("1 {}", shard_str)};
+                            egg_vendor_mansion.push(pokemon_egg);
+
+                        },
+                        None => break
+                    }
+                }
+            },
+            None => ()
+        }
+    column_number += 1;
+    }
+
+    for pokemon_cell in document.range((2,5),(15,5)).used_cells(){
+        match pokemon_cell.2.get_string(){
+            Some(pokemon) => {
+                let pokemon_egg: EggVendor = EggVendor { pokemon: replace_galarian_alola(pokemon), price: "$100 000 each, additional $100 000 for Shiny form.".to_string()};
+                egg_vendor_gamecorner.push(pokemon_egg);
+            },
+            None => break
+        }
+    
+
+    }
+    Some((egg_vendor_mansion, egg_vendor_gamecorner))
+}
+
+
+fn calamine_egg_vendor_extractor() -> Encounters<RouteInfo<EggEncounter>>{
+    let mut excel: Xlsx<_> = open_workbook("pokemon_locations.xlsx").expect("Couldn't open pokemon xlsx");
+    let sheets = excel.sheet_names().to_owned(); 
+    let mut route_vec: Vec<RouteInfo<EggEncounter>> = vec![];
+
+    if let Some(Ok(document)) = excel.worksheet_range(&sheets[4]){
+        match get_egg_vendor_pokemon(&document){
+            Some(tuple) => {
+                let egg_encounter:EggEncounter = EggEncounter { mansion: tuple.0, gamecorner: tuple.1 };
+                let route_info_mansion: RouteInfo<EggEncounter> = RouteInfo{ route: "Celadon".to_string(), encounter_info:egg_encounter };
+                route_vec.push(route_info_mansion);
+                //let route_info_gamecorner: RouteInfo<EggVendor> = RouteInfo{ route: "Celadon's Mansion".to_string(), encounter_info:tuple.1 };
+            }
+            None => (),
+        
+        }
+    }
+    let encounters:Encounters<RouteInfo<EggEncounter>> = Encounters { encountermethod: "Egg Vendor".to_string(), routes: route_vec };
+    encounters
+}
+
+fn get_fossil_pokemon( document: &calamine::Range<calamine::DataType>) -> FossilEncounter{
+
+    let mut trade_fossil_list:Vec<Fossil> = vec![];
+    let mut found_fossil_list:Vec<Fossil> = vec![];
+    let mut column_number = 0;
+
+    while column_number < 4{
+        let shard_type = get_cell_text(2, column_number, document);
+        
+        match shard_type{
+            Some(shard_str) => {
+                for pokemon_cell in document.range((3,column_number),(5,column_number)).used_cells(){
+                    match pokemon_cell.2.get_string(){
+                        Some(pokemon) => {
+                            let pokemon_split: Vec<_> = pokemon.split([' ', ','].as_ref()).collect();
+
+                            let pokemon_fossil: Fossil = Fossil {
+                                pokemon: replace_galarian_alola(pokemon_split[0]),
+                                price: format!("{} {}s", pokemon_split[1].replace("(", ""), shard_str.replace(":", "")), obs:"".to_string()};
+
+                            println!("{:?}", pokemon_fossil);
+                            trade_fossil_list.push(pokemon_fossil);
+
+                        },
+                        None => break
+                    }
+                }
+            },
+            None => ()
+        }
+    
+    column_number += 1;
+    }
+
+    found_fossil_list.push(Fossil{
+        pokemon: "Aerodactyl".to_string(),
+        price:"-".to_string(),
+        obs: "Aerodactyl's fossil is in Pewter's Museum back entrance, same as in vanilla Fire Red. A guy in the Mansion can restore it.".to_string()
+    });
+    found_fossil_list.push(Fossil{
+        pokemon: "Omanyte/Kabuto".to_string(),
+        price:"-".to_string(),
+        obs: "Omanyte and Kabuto's fossils are in Mt. Moon, same as in vanilla Fire Red. A guy in the Mansion can restore them\n
+        The guy in Celadon's Mansion gives you the other choice that you didn't pick in Mt. Moon..".to_string()
+    });
+
+
+    let fossil_encounter: FossilEncounter = FossilEncounter { trade: trade_fossil_list, found: found_fossil_list };
+    fossil_encounter
+}
+
+fn calamine_fossil_extractor() -> Encounters<RouteInfo<FossilEncounter>>{
+    let mut excel: Xlsx<_> = open_workbook("pokemon_locations.xlsx").expect("Couldn't open pokemon xlsx");
+    let sheets = excel.sheet_names().to_owned(); 
+    let mut route_vec: Vec<RouteInfo<FossilEncounter>> = vec![];
+    if let Some(Ok(document)) = excel.worksheet_range(&sheets[5]){
+        let fossil_encounter = get_fossil_pokemon(&document);
+        let route_info:RouteInfo<FossilEncounter> = RouteInfo { route: "Mansion".to_string(), encounter_info: fossil_encounter };
+        route_vec.push(route_info);
+        
+    }
+    let encounters:Encounters<RouteInfo<FossilEncounter>> = Encounters { encountermethod: "Egg Vendor".to_string(), routes: route_vec };
+    encounters
+}
+
+fn get_unobtainable_pokemon(){
 
 }
 
+fn calamine_unobtainable_pokemon() {
+    let mut excel: Xlsx<_> = open_workbook("pokemon_locations.xlsx").expect("Couldn't open pokemon xlsx");
+    let sheets = excel.sheet_names().to_owned(); 
+    if let Some(Ok(document)) = excel.worksheet_range(&sheets[5]){
+    
+    }
+
+}   
 
 fn main() {
 
@@ -567,7 +819,10 @@ fn main() {
     let water_encounter = calamine_water_extractor();
     let trade_encounters =calamine_trade_pokemon_extractor();
     let gift_encounters = calamine_gift_pokemon_extractor();
-    dbg!(gift_encounters);
+    let egg_counters = calamine_egg_vendor_extractor();
+    let fossil_encounters = calamine_fossil_extractor();
+
+    dbg!(water_encounter);
 
     
 }
